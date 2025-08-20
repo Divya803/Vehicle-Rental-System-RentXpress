a0c968d1ccea174d17e109dcb18f8c2fbcb8b852
Divyanjali Palpola <palpolatbd.21@uom.lk>
2025-08-21 03:40:04 +0530

    Reservation and driver dashboard updated

--- rentxpress/src/components/NavigationBar/NavigationBar.js
+++ rentxpress/src/components/NavigationBar/NavigationBar.js
@@ -154,7 +154,7 @@ const NavigationBar = ({ isLoggedIn = false }) => {
         <ul className="nav-links">
           <li><Link to="/" className="nav-link">Home</Link></li>
 
-          {isAuthenticated && userRole !== "Admin" && (
+          {/* {isAuthenticated && userRole !== "Admin" && (
             <>
               {userRole === "Driver" ? (
                 <li><Link to="/driver-dashboard" className="nav-link">Driver Dashboard</Link></li>
@@ -179,7 +179,40 @@ const NavigationBar = ({ isLoggedIn = false }) => {
               <li><Link to="/user-details" className="nav-link">Users</Link></li>
 
             </>
-          )}
+          )} */}
+          {isAuthenticated && userRole !== "Admin" && (
+    <>
+      {userRole === "Driver" && (
+        <li><Link to="/driver-dashboard" className="nav-link">Driver Dashboard</Link></li>
+      )}
+
+      {userRole === "user" && (
+        <>
+          <li><Link to="/user-dashboard" className="nav-link">User Dashboard</Link></li>
+          <li><Link to="/vehicles" className="nav-link">Vehicles</Link></li>
+          <li><Link to="/verify-account" className="nav-link">Verify Account</Link></li>
+        </>
+      )}
+
+      {userRole === "Vehicle Owner" && (
+        <>
+          <li><Link to="/user-dashboard" className="nav-link">User Dashboard</Link></li>
+            <li><Link to="/post-vehicle" className="nav-link">Postings</Link></li>
+          <li><Link to="/vehicles" className="nav-link">Vehicles</Link></li>
+        
+        </>
+      )}
+    </>
+  )}
+
+  {userRole === "Admin" && isAuthenticated && (
+    <>
+      <li><Link to="/admin-dashboard" className="nav-link">Dashboard</Link></li>
+      <li><Link to="/pending-reservations" className="nav-link">Reservations</Link></li>
+      <li><Link to="/pending-vehicle" className="nav-link">Pending Vehicles</Link></li>
+      <li><Link to="/user-details" className="nav-link">Users</Link></li>
+    </>
+  )}
         </ul>
       </div>
 
